76090e9c9194cbf984d25676041a6f5153fdd83d
Adrial-Knight <pierreminier64@gmail.com>
2022-12-06 15:57:25 +0100

    [Sim Singer] Factorisation calculs

--- src/generateurs/sim_singer.m
+++ src/generateurs/sim_singer.m
@@ -1,25 +1,21 @@
-function [X] = sim_singer(N, a, T, sigma2)
-%SIM_SINGER simule sur N points l'evolution d'un processus a deux
+function [X] = sim_singer(N, alpha, T, sigma2)
+%SIM_SINGER simule sur N points l'évolution d'un processus a deux
 %dimensions x et y selon les parametres a et T
 
 if ~exist('sigma2', 'var')
     sigma2 = 0.0;
 end
 
-X = zeros(3,N);
-mu = [0; 0; 0];
+nb_var_etat = 3;  % position, vitesse et accéleration
+X = zeros(nb_var_etat, N);
 
-phi = [1  T (exp(-a*T) + a * T - 1)/a^2;...
-    0  1      (1 - exp(-a*T))/a;     ...
-    0  0          exp(-a*T)         ];
+phi = get_phi(alpha, T);
+Q = get_Q(sigma2, alpha, T);
+A = chol(Q).';  % Q = A*(A^t)
 
 % mise a jour
 for i=1:N-1
-    X_tmp      = phi * X(:,i);
-    Q          = chol(get_Q(sigma2, a, T));
-    W          = mu + Q.'*randn(size(Q,2),1);
-    X(:,i+1) = X_tmp+W;
+    X_next = phi * X(:, i);
+    W      = A*randn(nb_var_etat, 1);
+    X(:, i+1) = X_next + [0; 0; 1] .* W;
 end
-
-
-
