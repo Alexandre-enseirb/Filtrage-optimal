635aea09d1a72ffdf29cd97b4b5868a36e1a4870
Alexandre <alvalade@enseirb-matmeca.fr>
2022-12-06 13:25:35 +0100

    Singer movement implemented

--- src/generateurs/sim_singer.m
+++ src/generateurs/sim_singer.m
@@ -1,18 +1,19 @@
 function [X] = sim_singer(N, a, T, sigma2)
-%SIM_SINGER simule sur N points l'evolution d'un processus a deux
-%dimensions x et y selon les parametres a et T
-
-if ~exist('sigma2', 'var')
-    sigma2=0;
-end
-
-X = zeros(6,N);
-mu = [0; 0; 0];
-
-for i=1:N-1
-    X_tmp = singer(X(:,i), a, T);
-    Q     = get_Q(sigma2, a, T);
-    R     = chol(Q);
-    W     = mu + [randn(size(X))*R; randn(size(X))*R];
-    X(:,i+1) = X_tmp+W;
-end
+    %SIM_SINGER simule sur N points l'evolution d'un processus a deux
+    %dimensions x et y selon les parametres a et T
+    
+    if ~exist('sigma2', 'var')
+        sigma2 = struct("x", 0.0, "y", 0.0);
+    end
+    
+    X = zeros(6,N);
+    mu = [0; 0; 0];
+    
+    for i=1:N-1
+        X_tmp = update_singer(X(:,i), a, T);
+        Q_x   = get_Q(sigma2.x, a, T);
+        Q_y   = get_Q(sigma2.y, a, T);
+        R     = [chol(Q_x);chol(Q_y)];
+        W     = repmat(mu,2,1) + [R*randn(size(R,2),1)];% R*randn(size(R,2),1)];
+        X(:,i+1) = X_tmp+W;
+    end
--- src/main.m
+++ src/main.m
@@ -11,6 +11,18 @@ sigma_2m = struct("x", 1.2, "y", 0.9);
 
 %%
 mu = [0, 0, 0];
-Q = get_Q(sigma_2m.x, alpha, T);
-R = chol(Q);
-z = repmat(mu, N, 1) + randn(N, 3)*R;
+%Q = get_Q(sigma_2m.x, alpha, T);
+%R = chol(Q);
+%z = repmat(mu, N, 1) + randn(N, 3)*R;
+
+X = sim_singer(N, alpha, T, sigma_2m);
+
+X_pos_x = X(1,:);
+X_pos_y = X(4,:);
+
+figure
+plot(X_pos_x, X_pos_y, 'Color', '#4DBEEE', 'LineStyle','--');
+xlabel("X position");
+ylabel("Y position");
+legend("Singer movement");
+title("Simulation of a Singer movement over 500 iterations.");
