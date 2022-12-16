2fd399118485662cbed80f13340e939e51fe9b2a
Adrial-Knight <pierreminier64@gmail.com>
2022-12-16 17:42:43 +0100

    [Getter]
    utilisation de la fonction get_R au lieu d'ecrire explicitement la formule

--- src/kalman_nominal.m
+++ src/kalman_nominal.m
@@ -6,9 +6,13 @@ addpath("generateurs/", "filtrage");
 params.alpha = 1;  % croissance de la fonction d'auto-corrélation
 params.N = 500;    % nombre d'observations
 params.T = 1;      % période d'echantillonage
+
 % variance de l'acceleration en x et en y a l'instant 0
 params.sigma_2m = struct("x", 1.2, "y", 0.9);
 nb_var_etat = 3;
+
+% bruit de mesure
+sigma2_measure = 200;
 
 %% Une realisation
 % simulation d'un mouvement singer
@@ -16,7 +20,7 @@ X.x = sim_singer(params.N, params.alpha(1), params.T, params.sigma_2m.x);
 X.y = sim_singer(params.N, params.alpha(1), params.T, params.sigma_2m.y);
 
 % ajout du bruit d'observation
-R = 200*eye(nb_var_etat);
+R = get_R(sigma2_measure, nb_var_etat);
 Y.x = X.x + R * rand(size(X.x));
 Y.y = X.y + R * rand(size(X.x));
 
